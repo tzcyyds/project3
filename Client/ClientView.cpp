094484afe3740308d67013544e5f28d8c41aca52
yudipan <3010201649@qq.com>
2021-10-31 14:59:38 +0800

    添加项目文件。

---
+++ Client/ClientView.cpp
@@ -0,0 +1,82 @@
+﻿
+// ClientView.cpp: CClientView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Client.h"
+#endif
+
+#include "ClientDoc.h"
+#include "ClientView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CClientView
+
+IMPLEMENT_DYNCREATE(CClientView, CView)
+
+BEGIN_MESSAGE_MAP(CClientView, CView)
+END_MESSAGE_MAP()
+
+// CClientView 构造/析构
+
+CClientView::CClientView() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CClientView::~CClientView()
+{
+}
+
+BOOL CClientView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CClientView 绘图
+
+void CClientView::OnDraw(CDC* /*pDC*/)
+{
+	CClientDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CClientView 诊断
+
+#ifdef _DEBUG
+void CClientView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CClientView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CClientDoc* CClientView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CClientDoc)));
+	return (CClientDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CClientView 消息处理程序
